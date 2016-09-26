4a2d789126f9f69f30b7de025cda8d6e372f8ed0
lijianli <lijianli@conew.com>
2016-09-26 13:24:54 +0800

    quick sort.

--- Algorithm/src/com/algorithm/sort/TestQuickSort.java
+++ Algorithm/src/com/algorithm/sort/TestQuickSort.java
@@ -2,25 +2,25 @@ package com.algorithm.sort;
 
 public class TestQuickSort {
     @SuppressWarnings("unchecked")
-    private static int Partion(Object [] arr,int low,int high){
-        Object pivotkey=arr[low];
+    private static int Partition(Object [] arr,int low,int high){
+        Object pivotKey=arr[low];
         while(low<high){
-            while(low<high&&((Comparable<Object>)arr[high]).compareTo((Comparable<Object>)pivotkey)>=0){
+            while(low<high&&((Comparable<Object>)arr[high]).compareTo(pivotKey)>=0){
                 --high;
             }
             arr[low]=arr[high];
-            while(low<high&&((Comparable<Object>)pivotkey).compareTo((Comparable<Object>)arr[low])>=0){
+            while(low<high&&((Comparable<Object>)pivotKey).compareTo(arr[low])>=0){
                 ++low;
             }
             arr[high]=arr[low];
         }
-        arr[low]=pivotkey;
+        arr[low]=pivotKey;
         return low;
     }
     
     public static void QuickSort(Object arr[],int low,int high){
         if(low<high){
-            int pivot=Partion(arr, low, high);
+            int pivot=Partition(arr, low, high);
             QuickSort(arr, low, pivot-1);
             QuickSort(arr, pivot+1, high);
         }
@@ -28,8 +28,8 @@ public class TestQuickSort {
     
     public static void main(String args[]){
 //        Object arr[]={1,3,4,2,0,-5,6,7}; 
-//        Object arr[]={1.1,3.3,4.2,2.2,0.1,-5.0,6.99999,7.0}; 
-        Object arr[]={"how","are","you","i","am","fine","thanks","!"}; 
+        Object arr[]={1.1,3.3,4.2,2.2,1.1,-5.0,6.99999,7.0};
+//        Object arr[]={"how","are","you","i","am","fine","thanks","!"};
 //        Object arr[]={"123","432","213","124","a123","432","23412","adfa"}; 
         QuickSort(arr,0,arr.length-1);
         for(int i=0;i<arr.length;i++){
